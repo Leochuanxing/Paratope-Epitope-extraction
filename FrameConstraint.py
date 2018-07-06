d571d753aef6fdf3a343f3ca175e41bd77496b01
Leo Chuan <17210180030@fudan.edu.cn>
2018-07-06 22:37:04 +0800

    To some extent, the ordering problem of the complementary amino acids is solved

--- FrameConstraint.py
+++ FrameConstraint.py
@@ -5,7 +5,7 @@ import json
 with open('ac_contact', 'r') as f:
     ac_contact = json.load(f)
 ac_contact.keys() 
-ac_contact['1deeFh']
+ac_contact['1adqHh']
 #########################################################
 # define a finction, to find the consetutive aa in the ref frame with a given 
 # lenth
@@ -50,6 +50,7 @@ def Max_ctn_aa(ac_contact, length, ref_chain = 'Ag'):
             for fcdn in temp_dict[cdr]:
                 if fcdn[ind] not in temp_aapos:
                     temp_aapos.append(fcdn[ind])
+            '''The order of the reference aa is decided below'''
             #sort temp_aapos for the convenience to find the consecutive aa
             temp_aapos.sort()
             
@@ -112,6 +113,11 @@ inputs: max_ctn_aa, a dictionary returned from 'Max_ctn_aa'
        ac_contact, a dictionary returned from the AlignmentConstraint module
        seq, a dictionary, give all the sequences.{'1adq'{'A':['ALA', 'GLU', ...]...}}
        CN_gate, an integer, set a lower limit for an aa to be inluded in the returns
+       mode, a string, takes the value of 'consecutive' or 'largest'
+          'consecutive' means find the consecutive complementary aas with length 'complement_length'
+           and the consecutive aas are with the largest contact among the same type
+           'largest' find the complement aas with lenth no bigger than 'complement_length', and with 
+            the largest contact number among the same type.
 return:parepi_pos, a dictionary in the form of   {'1adqHh': ['h1HA', [30], [16, 17], 12, 17],.. }
           'h1HA' means CDRh1, antibody chain is 'H', antigen chain is 'A';
           [30], gives the positions of the consecutive paratope aas no longer than 'complement_length'
@@ -121,7 +127,8 @@ return:parepi_pos, a dictionary in the form of   {'1adqHh': ['h1HA', [30], [16, 
       parepi_aa, a corresponding dictionary of parepi_pos, the only difference is the pos is translated
           into amino acids.
 '''
-def ParaEpi(max_ctn_aa, ac_contact, seq, complement_length, CN_gate = 1):
+def ParaEpi(max_ctn_aa, ac_contact, seq, complement_length, CN_gate = 1, mode = 'consecutive'):
+    assert mode in ['largest', 'consecutive'], 'Wrong mode value!'
     # Creat empty containers for the final results
     parepi_pos = {}; parepi_aa = {}
     # extract the ref_chain information from max_ctn_aa(Now I feel the advantage of class)
@@ -154,24 +161,88 @@ def ParaEpi(max_ctn_aa, ac_contact, seq, complement_length, CN_gate = 1):
         for max_ctn in max_ctn_aa[Ab_chian]:
             # we should guranttee that max_ctn[2] exists
             if max_ctn[2] != None:
-                # Into one CDR of the CN_gated_dict
-                ''' we just throw away the least contact ones if the c_aa is too long'''
-                # in order to do this , sort the list of fcdn first
-                CN_gated_dict[max_ctn[0]].sort(key = lambda x:x[3], reverse = True)
-                # extract all the completmentary positions contact with max_ctn
-                # and store them in 'temp_c_pos'
-                temp_c_pos = []
-                for fcdn in CN_gated_dict[max_ctn[0]]:
-                    if len(temp_c_pos) < complement_length and fcdn[ind] in max_ctn[2]:
+                # here we can creat two modes, one is to calculate find the complementary
+                # aas with the most contact. The other is to find the consecutive complementary
+                # aas with teh most contact.
+                # for the consecutive ones we want to use the 'Max_ctn_aa' function
+                '''As to the complementary aa, we want to stick to the idea
+                    that short consecutive aas plays an important role'''
+                if mode == 'consecutive':
+                    # mimic ac_contact
+                    mimic_ac_contact = {}
+                    mimic_ac_contact[Ab_chian] = []
+                    for fcdn in CN_gated_dict[max_ctn[0]]:
+                        if fcdn[ind] in max_ctn[2]:
+                            mimic_ac_contact[Ab_chian].append(fcdn)
+                    # find the mimic_ref_chain
+                    if ref_chain == 'Ab':
+                        mimic_ref_chain = 'Ag'
+                    if ref_chain == 'Ag':
+                        mimic_ref_chain = 'Ab'                    
+                    #  apply 'Max_ctn_aa' function
+                    mimic_max_ctn_aa = Max_ctn_aa(mimic_ac_contact, complement_length, mimic_ref_chain )
+                    temp_c_pos = mimic_max_ctn_aa[Ab_chian][0][2]
+                    # What if the order is reversed?
+                    '''Here we give it a simple criteria, if the refernce frame has
+                    more than one raa, take the first complementary caa,  calculate the
+                    contact between this caa and the first raa, and the contact number is c1
+                    then calculate the contact number between this caa and the last raa, and the 
+                    contact number is c2, if c1>c2, keep the order of caa, if c1<c2, reverse the order of 
+                    caa'''
+                    #temp_c_pos, as the result of the function 'Max_ctn_aa' is already sorted
+                    # and temp_c_pos, as the result of the function 'Max_ctn_aa', it may be None
+                    if temp_c_pos != None and len(temp_c_pos) > 1 and len(max_ctn[0]) > 1 :
+                        c1 = 0 ; c2 = 0 
+                        for fcdn in CN_gated_dict[max_ctn[0]]:
+                            if fcdn[ind] == max_ctn[0][0] and fcdn[c_ind] == temp_c_pos[0]:
+                                c1 = fcdn[3]
+                            if fcdn[ind] == max_ctn[0][-1] and fcdn[c_ind] == temp_c_pos[0]:
+                                c2 = fcdn[3]  
+                        if c1 < c2:
+                            temp_c_pos.sort(reverse = True)
+                #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+                # the other mode
+                if mode == 'largest':
+                    # in order to do this , sort the list of fcdn first
+                    CN_gated_dict[max_ctn[0]].sort(key = lambda x:x[3], reverse = True)
+                    # extract all the completmentary positions contact with max_ctn
+                    # and store them in 'temp_c_pos'
+                    '''Here we have to consider the order of the elements in 'temp_c_pos', make
+                    sure it jibes with the order of the aa in the reference frame'''
+                    # our strategy is to collect all the fcdn meet the requirement of the 
+                    # CN_gate and the complement_length. Do it! But first we have to 
+                    # collect temp_c_pos, then collect the fcdns according to temp_c_pos
+                    
+                    temp_c_pos = []
+                    for fcdn in CN_gated_dict[max_ctn[0]]:
+                        if len(temp_c_pos) < complement_length and fcdn[ind] in max_ctn[2]:
+                            if fcdn[c_ind] not in temp_c_pos:
+                               temp_c_pos.append(fcdn[c_ind])
+                #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+                # this block is to adjust the order of the complementary aas
+                    '''whether this block is applied to mode largest or both is a question'''
+                    # Collect the fcdn 
+                    temp_fcdn = []
+                    for fcdn in CN_gated_dict[max_ctn[0]]:
+                        if fcdn[c_ind] in temp_c_pos:
+                            temp_fcdn.append(fcdn)
+                    temp_fcdn
+                    # sort the temp_fcdn according to the order of the reference chain
+    
+                    temp_fcdn.sort(key = lambda x:x[ind])
+                    # draw the temp_c_pos again
+                    temp_c_pos = []
+                    for fcdn in temp_fcdn:
                         if fcdn[c_ind] not in temp_c_pos:
-                            temp_c_pos.append(fcdn[c_ind])
-                temp_c_pos.sort()
+                           temp_c_pos.append(fcdn[c_ind])
+                #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 #                calculate the number of contacts between the c_aas and the 
 #                 creat a contact recorder 'c'
                 c = 0
-                for fcdn in CN_gated_dict[max_ctn[0]]:
-                    if fcdn[ind] in max_ctn[2] and fcdn[c_ind]  in temp_c_pos :
-                        c += fcdn[3]
+                if temp_c_pos != None:
+                    for fcdn in CN_gated_dict[max_ctn[0]]:
+                        if fcdn[ind] in max_ctn[2] and fcdn[c_ind]  in temp_c_pos :
+                            c += fcdn[3]
                 # Everything is prepared, load the data
                 if ref_chain == 'Ab':
                    parepi_pos[Ab_chian].append([max_ctn[0], max_ctn[2], temp_c_pos, c, max_ctn[4]])
@@ -190,19 +261,20 @@ def ParaEpi(max_ctn_aa, ac_contact, seq, complement_length, CN_gate = 1):
                 # creat temporary containers
                 temp_c_aa, temp_max_ctn_aa  = [], []
                 # load the the 'temp_c_aa', but we have to calculate whether there are jumps
-                temp_c_pos.sort()# can not sort this way, has to sort according to 
                 # creat an indicator to show if ther are jumps
-                indicator = temp_c_pos[0]
-                temp_c_aa.append(seq[Ab_chian[:4]][c_cid][indicator])
-                if len(temp_c_pos) >= 2:
-                    for pos in range(1, len(temp_c_pos)):
-                        if temp_c_pos[pos] - indicator >= 2:
-                            temp_c_aa.append(str(temp_c_pos[pos] - indicator-1))
-                            temp_c_aa.append(seq[Ab_chian[:4]][c_cid][temp_c_pos[pos]])
-                            indicator = temp_c_pos[pos]
-                        else:
-                            temp_c_aa.append(seq[Ab_chian[:4]][c_cid][temp_c_pos[pos]])
-                            indicator = temp_c_pos[pos]
+                if temp_c_pos != None:
+                    indicator = temp_c_pos[0]
+                    temp_c_aa.append(seq[Ab_chian[:4]][c_cid][indicator])
+                    if len(temp_c_pos) >= 2:
+                        for pos in range(1, len(temp_c_pos)):
+                            # don't distinguish N end or C end as long as they next to each other
+                            if abs(temp_c_pos[pos] - indicator) >= 2:
+                                temp_c_aa.append(temp_c_pos[pos] - indicator)
+                                temp_c_aa.append(seq[Ab_chian[:4]][c_cid][temp_c_pos[pos]])
+                                indicator = temp_c_pos[pos]
+                            else:
+                                temp_c_aa.append(seq[Ab_chian[:4]][c_cid][temp_c_pos[pos]])
+                                indicator = temp_c_pos[pos]
                 # load the the 'max_ctn_aa', don't have to calculate jumps
                 for pos in max_ctn[2]:
                     temp_max_ctn_aa .append(seq[Ab_chian[:4]][cid][pos])
@@ -212,20 +284,23 @@ def ParaEpi(max_ctn_aa, ac_contact, seq, complement_length, CN_gate = 1):
                 if ref_chain == 'Ag':
                     parepi_aa[Ab_chian].append([max_ctn[0], temp_c_aa, temp_max_ctn_aa , c, max_ctn[4]])
     # Clean the house before left               
-    del CN_gated_dict, temp_c_pos, c, temp_c_aa, temp_max_ctn_aa   
+    del CN_gated_dict, temp_c_pos, c, temp_c_aa, temp_max_ctn_aa
 #    return indicator
     return parepi_pos, parepi_aa
+
 # Check if it works
 with open('sequence_older', 'r') as f:
     sequence = json.load(f)
-parepi_pos, parepi_aa = ParaEpi(max_ctn_aa, ac_contact, seq =sequence, complement_length = 10, CN_gate = 1)
+parepi_pos, parepi_aa = ParaEpi(max_ctn_aa, ac_contact, seq =sequence, complement_length = 2, CN_gate = 1, mode = 'consecutive')
 parepi_aa.keys()           
 parepi_aa['5vebAh'] 
-ParaEpi_framed['5veb']['h1AX']
+ParaEpi_framed['5veb']
 ParaEpi_framed['5veb']['h2AX']
 ParaEpi_framed['5veb']['h3AX']
-parepi_pos['5vebAh']['h1AX']
-
+parepi_pos['5vebAh']
+keys = list(parepi_pos.keys())
+for i in keys[:5]:
+    print(parepi_pos[i])
 ParaEpi_pos_framed['5veb']
 sequence['5veb']['A'][102]         
 #for AB_c in max_ctn_aa:
